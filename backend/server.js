cb15b13da0437c8aaf3dbbd126dc6fded0c3ef7e
irmikimikmik <irmakb@students.cs.ubc.ca>
2024-04-06 13:58:04 -0700

    still setting up

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const app = express();
 const port = process.env.PORT || 3000;
 const cors = require('cors');
 require('dotenv').config();
 
-
+const app = express();
 app.use(cors());
 app.use(express.json());
 
@@ -16,15 +15,22 @@ app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
 
+// !!!/api/data
 app.get('/api/data', (req, res) => {
     // You would fetch data from database or another service
     const data = { message: 'This is your data' };
     res.json(data);
 });
 
+// !!!/api/data
 app.post('/api/data', (req, res) => {
     // You would save data to database or pass it to another service
     const newData = req.body;
     console.log(newData);
     res.status(201).send('Data created');
+});
+
+app.post("/out", (req, res) => {
+    res.status(200);
+    res.send("OK");
 });
