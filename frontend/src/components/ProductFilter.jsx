d4ab83e7729f4cc25ec22900527b2c5d76ff1d1c
selinuz <92901412+selinuz@users.noreply.github.com>
2024-04-07 11:48:19 -0700

    product filter added

--- frontend/src/components/ProductFilter.jsx
+++ frontend/src/components/ProductFilter.jsx
@@ -1,14 +1,38 @@
-export default function Filter(onSortChange) {
+export default function Filter({ onSortChange }) {
     return (
         <div className="flex justify-between items-center mb-4">
             <div className="flex gap-4 items-center">
-                <span className="ml-8">Product</span>
+                <select className="ml-2 p-1 border rounded">
+                    <option value="all">All Products</option>
+                    <option value="jackets">Jackets</option>
+                    <option value="shoes">Shoes</option>
+                    <option value="shoes">Pants</option>
+                </select>
                 <div className="border-l h-5 border-gray-400"></div> {/* Thin vertical line */}
-                <span>Color</span>
+                <select className="ml-2 p-1 border rounded">
+                    <option value="all">All Ratings</option>
+                    <option value="4stars">4 Stars & Up</option>
+                    <option value="3stars">3 Stars & Up</option>
+                    {/* Add more rating options as needed */}
+                </select>
+                <div className="border-l h-5 border-gray-400"></div> {/* Thin vertical line */}            
+        
+                <select className="ml-2 p-1 border rounded">
+                    <option value="all">My Colors</option>
+                    <option value="black">Black</option>
+                    <option value="white">White</option>
+                    <option value="white">Red</option>
+                    <option value="white">Blue</option>
+                </select>
                 <div className="border-l h-5 border-gray-400"></div> {/* Thin vertical line */}
-                <span>Rating</span>
-                <div className="border-l h-5 border-gray-400"></div> {/* Thin vertical line */}
-                <span>Size</span>
+                
+                <select className="ml-2 p-1 border rounded">
+                    <option value="all">My Size</option>
+                    <option value="small">Small</option>
+                    <option value="medium">Medium</option>
+                    <option value="large">Large</option>
+                    <option value="large">XLarge</option>
+                </select>
             </div>
             <div>
                 <label htmlFor="sort">Sort:</label>
