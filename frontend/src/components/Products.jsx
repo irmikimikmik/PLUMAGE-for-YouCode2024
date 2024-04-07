a87080eb117be42126105637c03bfeb152c5dd10
Iris Xu <irisxu02@gmail.com>
2024-04-07 09:47:27 -0700

    Add stores to popup

--- frontend/src/components/Products.jsx
+++ frontend/src/components/Products.jsx
@@ -1,7 +1,14 @@
 import Product from "@/components/Product";
 import React, { useEffect, useState } from 'react';
+<<<<<<< HEAD
 import Filter from "@/components/ProductFilter";
 import styles from "@/styles/products.css";
+=======
+import Filter from "@/components/ProductFilter"
+import Image from "next/image";
+import stores_list from "../../public/stores.png";
+import styles from "@/styles/products.css"
+>>>>>>> bb77a53d (Add stores to popup)
 
 export default function Products() {
     const [products, setProducts] = useState([]);
@@ -9,6 +16,7 @@ export default function Products() {
     const [useSelectedColorsFilter, setUseSelectedColorsFilter] = useState(false); // Step 1: Toggle state
     const [showPopup, setShowPopup] = useState(false);
     const [selectedProduct, setSelectedProduct] = useState(null);
+    const [showIframe, setShowIframe] = useState(true);
     const arcteryxColorOptions = [
         "Green", "Tatsu", "Blue", "Black", "Light Vitality", "Orange", "Grey", "Edziza", "Void",
         "Stone Wash", "Chloris", "Solitude II", "Graphite", "Solitude", "Forage", "Black Sapphire",
@@ -58,6 +66,10 @@ export default function Products() {
         setShowPopup(!showPopup);
     };
 
+    const toggleContent = () => {
+        setShowIframe(!showIframe);
+    };
+    
     return (
         <div className="w-full pt-11">
             <h2>Recommended For You</h2>
@@ -74,8 +86,17 @@ export default function Products() {
                 <div className="popup">
                     <div className="popup_inner">
                         <h1>{selectedProduct.name}</h1>
-                        <iframe src="https://rte-web-viewer.substance3d.com/model/https%3A%2F%2Fcdn.substance3d.com%2Fv2%2Ffiles%2Fshare%2F08ad1941-d289-4f7f-a7de-c2ced88ff4a2%3Fencrypted%3Dtrue" width="100%" height="500" frameborder="0" allow="fullscreen"></iframe>
-
+                        <div className="content-wrapper">
+                            {showIframe ? (
+                                <iframe src="https://rte-web-viewer.substance3d.com/model/https%3A%2F%2Fcdn.substance3d.com%2Fv2%2Ffiles%2Fshare%2F08ad1941-d289-4f7f-a7de-c2ced88ff4a2%3Fencrypted%3Dtrue" width="50%" height="500" frameBorder="0" allowFullScreen></iframe>
+                            ) : (
+                                <img src={selectedProduct.imageUrl} alt="Product Image" />
+                            )}
+                            <button onClick={toggleContent}>
+                                {showIframe ? 'Show Static Image' : 'Show 3D'}
+                            </button>
+                            <Image width="10%" src={stores_list} alt="Company Logo" id="stores_list" />
+                        </div>
                         <button onClick={() => setShowPopup(false)}>Close</button>
                     </div>
                 </div>
