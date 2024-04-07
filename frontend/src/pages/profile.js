cf24a9d923a01f422ba45db9b039ce2beb7b5b58
irmikimikmik <irmakb@students.cs.ubc.ca>
2024-04-07 04:58:25 -0700

    fix node modules

---
+++ frontend/src/pages/profile.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const ProfilePage = () => {
+    // This is a mock React Component due to the following build error:
+    // Build optimization failed: found page without a React Component as default export in pages/profile
+    // Feel free to remove this code so long there is a React Component on this page
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      {/* Your profile page content goes here */}
+    </div>
+  );
+};
+
+export default ProfilePage;
